f221579d27701ddbf18ca96167e0cf996fa11d7b
Ruth Moraa <moraar37@gmail.com>
2025-02-09 06:18:49 +0300

    adding assosciative arrays

--- 03_arrays.php
+++ 03_arrays.php
@@ -6,4 +6,12 @@ $fruits = array ("apple", "melon", "lemon");
 echo $numbers;
 print_r($fruits);
 echo $fruits[1];
+
+//associative arrays
+
+$hex = [
+    'red' => "#foo",
+    'blue' => "0f0",
+    'green' => "#oof"
+];
 ?>
