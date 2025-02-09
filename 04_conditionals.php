278ccbdbfbad08a903569d405cc09c5cfeaba5da
Ruth Moraa <moraar37@gmail.com>
2025-02-09 06:37:16 +0300

    adding conditionals

---
+++ 04_conditionals.php
@@ -0,0 +1,11 @@
+<?php
+// < less than
+// > greater than
+// <= less than or equal to
+// >= greater than or equal to
+// == equal  to
+// === identical to
+// != not equal to
+// !not identical to
+
+?>
